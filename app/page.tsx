04bb085631f8aa36e28ebe2f5cb118676b7760fa
donesteban <stefan.riegel@telig.de>
2024-10-08 16:36:08 +0100

    Update something and deploy again.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
   const base = path.join(process.cwd(), "contentsx4", "hersteller");
   return (
     <div>
-      <h1>TEST</h1>
+      <h1>TEST at 24/10/08</h1>
       <p>Base path: {base}</p>
     </div>
   );
