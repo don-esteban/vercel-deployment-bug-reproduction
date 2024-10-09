9985d28e7ca182ac53232b3fc3b71ddb7aa8f72a
donesteban <stefan.riegel@telig.de>
2024-10-09 14:05:11 +0100

    Add request time rendering

---
+++ app/[...slug]/page.tsx
@@ -0,0 +1,15 @@
+import path from "path";
+import fs from "fs";
+
+export default async function Page() {
+  const base = `${process.cwd()}/contentsx4/hersteller`
+  const file = fs.readFileSync(path.join(base, 'abbyy', 'index.mdx'), 'utf8') // Can't access the file
+  return (
+    <div>
+      <h1>Request time redering</h1>
+      <div>
+      {file}
+      </div>
+    </div>
+  );
+}
