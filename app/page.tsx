5ba587362b54e7c70d1767e0b0ba15c7adaef58f
donesteban <stefan.riegel@telig.de>
2024-10-09 08:58:33 +0100

    Implement proposal from Vikhyath fixing the typo.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import path from "path";
 
 export default async function Page() {
-  const base = `${process.cwd()}/contentsx/hersteller`;
+  const base = `${process.cwd()}/contentsx4/hersteller`; // Observe the typo. It should be "contentsx4" instead of "contentsx"
+//  const base = `/vercel/path0/contentsx4/hersteller`
   return (
     <div>
       <h1>TEST at 24/10/08</h1>
