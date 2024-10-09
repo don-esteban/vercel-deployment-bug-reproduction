5ee1c44bf92b4524827fbeb1a8a1e0c3c121f9f3
donesteban <stefan.riegel@telig.de>
2024-10-09 12:40:14 +0100

    Add file access

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import path from "path";
+import fs from "fs";
 
 export default async function Page() {
-  const base = '/vercel/path0/contentsx4/hersteller'
-  const secondBase = base // If we remove this line, the project deploys successfully.
+  const base = `${process.cwd()}/contentsx4/hersteller`
+  const file = fs.readFileSync(path.join(base, 'abbyy', 'index.mdx'), 'utf8') // Can't access the file
   return (
     <div>
-      <h1>TEST at 24/10/08</h1>
+      <h1>TEST at 25/10/08</h1>
+      <div>
+      {file}
+      </div>
     </div>
   );
 }
