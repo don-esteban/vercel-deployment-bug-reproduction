355a5e2711ebd98a46e2cc63ed59bd43e86b8fd2
donesteban <stefan.riegel@telig.de>
2024-10-09 09:12:44 +0100

    Assign the path to base and assign base to secondBase

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import path from "path";
 export default async function Page() {
 //  const base = `${process.cwd()}/contentsx4/hersteller`; // Observe the typo. It should be "contentsx4" instead of "contentsx"
   const base = '/vercel/path0/contentsx4/hersteller'
+  const secondBase = base
   return (
     <div>
       <h1>TEST at 24/10/08</h1>
