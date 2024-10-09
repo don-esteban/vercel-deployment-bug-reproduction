a746c775bdb94881a276eb8f7c197277f3ae606a
donesteban <stefan.riegel@telig.de>
2024-10-09 09:20:12 +0100

    Show most minimal reproduction I can come up with.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import path from "path";
 
 export default async function Page() {
-//  const base = `${process.cwd()}/contentsx4/hersteller`; // Observe the typo. It should be "contentsx4" instead of "contentsx"
   const base = '/vercel/path0/contentsx4/hersteller'
-  const secondBase = base
+  const secondBase = base // If we remove this line, the project deploys successfully.
   return (
     <div>
       <h1>TEST at 24/10/08</h1>
-      {/* <p>Base path: {base}</p> */}
-      <p>Path to project: {process.cwd()}</p>
     </div>
   );
 }
