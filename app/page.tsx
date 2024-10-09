bbe2340aa5ea7291eb248b041aacbf44c06a605c
donesteban <stefan.riegel@telig.de>
2024-10-09 08:44:59 +0100

    Implement proposal from Vikhyath. Observe: It will deploy due to a typo.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import path from "path";
 
 export default async function Page() {
-  const base = path.join(process.cwd(), "contentsx4", "hersteller");
+  const base = `${process.cwd()}/contentsx/hersteller`;
   return (
     <div>
       <h1>TEST at 24/10/08</h1>
       <p>Base path: {base}</p>
+      <p>Path to project: {process.cwd()}</p>
     </div>
   );
 }
