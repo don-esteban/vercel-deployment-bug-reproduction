692eaf3e507f685405af95af672110ca59b906e0
donesteban <stefan.riegel@telig.de>
2024-10-24 17:43:29 +0100

    Revert to most basic issue reproduction possible

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import path from "path";
-import fs from "fs";
-
 export default async function Page() {
   const base = `${process.cwd()}/contentsx4/hersteller`
-  const file = fs.readFileSync(path.join(base, 'abbyy', 'index.mdx'), 'utf8') // Can't access the file
+  console.log('base: ', base)
   return (
     <div>
-      <h1>TEST at 25/10/08</h1>
-      <div>
-      {file}
-      </div>
+      <h1>TEST at 24/10/24</h1>
     </div>
   );
 }
